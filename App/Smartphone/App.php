0df63593df18a4c1b22fc4bf237725cc204d067c
Davidbagus-Web <davidbagus154@gmail.com>
2022-09-19 13:01:26 +0700

    first commit

---
+++ App/Smartphone/App.php
@@ -0,0 +1,19 @@
+<?php
+
+class App {
+    public $daftarHape = [];
+
+    public function tambahTipe(Smartphone $produk) {
+        $this->daftarHape[] = $produk;
+    }
+
+    public function cetak() {
+        $str = "DAFTAR HAPE : <br> ";
+
+        foreach( $this->daftarHape as $p) {
+            $str .= "- {$p->getInfoHape()} <br>";
+        }
+
+        return $str;
+    }
+}
---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+require_once 'App/init.php';
+
+$XiaomiNote9 = new Xiaomi("Xiaomi", "Note 9", 0);
+$XiaomiNote10 = new Xiaomi("Xiaomi", "Note 10", 0);
+
+$SamsungGalaxy = new Samsung("Samsung", "Samsung Galaxy Z Flip4", 0);
+
+$OppoRealme = new Oppo("Oppo", "Oppo Realme 9 Pro", 0);
+
+$daftarHape = new App();
+$daftarHape->tambahTipe($XiaomiNote9);
+$daftarHape->tambahTipe($XiaomiNote10);
+$daftarHape->tambahTipe($SamsungGalaxy);
+$daftarHape->tambahTipe($OppoRealme);
+echo $daftarHape->cetak();
+
+?>
